60b9b09971ed263d718a56222f8c3787ff71c734
Fredrik Stålnacke <fredrik.stalnacke@omegapoint.se>
2015-03-13 08:40:01 +0100

    Changed the google docs reference to a reference instead of a url

--- megaquiz/www/js/services.js
+++ megaquiz/www/js/services.js
@@ -4,7 +4,8 @@ angular.module('starter.services', [])
   // Might use a resource here that returns a JSON array
 
   // TODO Implement as in https://docs.angularjs.org/tutorial/step_11
-  var quizSpreadSheet = "https://docs.google.com/spreadsheets/d/1DTeGI9jOG9cGZ0fh42WYLcJ_AUOeLpqH3-5zD_BElEg/edit?usp=sharing";
+  // https://github.com/times/angular-tabletop
+  var quizSpreadSheet = "1DTeGI9jOG9cGZ0fh42WYLcJ_AUOeLpqH3-5zD_BElEg";
 
   return {
 
