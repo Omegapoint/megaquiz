9571486935306988cfd8f82d1442ec0d2a803c35
Fredrik Forsberg <fredrik.forsberg@omegapoint.se>
2015-04-10 19:18:07 +0200

    quiz question page fixed, result page fixed, score calculated

--- megaquiz/www/app/app.module.js
+++ megaquiz/www/app/app.module.js
@@ -88,6 +88,11 @@ angular
       })
 
       .state('tab.quiz-question-result', {
+        params: {
+          'submittedAnswers': [], 
+          'questions': {},
+          'questionAnswers': []
+        },
         views: {
           'tab-quiz': {
             templateUrl: 'app/quiz-question/tab-quiz-result.html',
